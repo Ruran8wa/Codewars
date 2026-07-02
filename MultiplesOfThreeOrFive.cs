3e16e6c1d86850d7c5bed339a796610345e99553
ruran8wa <p.rurangwa@alustudent.com>
2026-07-02 12:08:05 +0200

    Multiples of 3 or 5 with C#

---
+++ MultiplesOfThreeOrFive.cs
@@ -0,0 +1,20 @@
+public static class Kata
+{
+    public static int Solution(int value)
+    {
+        int sum = 0;
+        if(value < 0) return 0;
+        for(int i=1; i < value; i++){
+            if(i % 5 == 0 && i % 3 == 0){
+                sum += i;
+            }else if(i % 5 == 0){
+                sum += i;
+            }else if(i % 3 == 0){
+                sum += i;
+            }else{
+                continue;
+            }
+        }
+        return sum;
+    }
+}
